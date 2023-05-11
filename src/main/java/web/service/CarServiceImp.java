02a47c6840717f30ed06b5708a1ac95af5e51e53
Ainazik <akylbekovaai.1998@gmail.com>
2023-05-11 21:04:43 +0300

    third commit

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -3,19 +3,20 @@ import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class CarServiceImp implements CarService{
 
-    List<Car> cars = new ArrayList<>();
+    List<Car> cars = Arrays.asList(new Car(1, "Ferrari", 1989),
+                    new Car(2, "BMW", 2021),
+                    new Car(3, "Audi", 2010),
+                    new Car(4, "Bently", 2000),
+                    new Car(5, "Acura", 1999));
+
     @Override
     public List<Car> listCars(Integer count) {
-        cars.add(new Car(1, "Ferrari", 1989));
-        cars.add(new Car(2, "BMW", 2021));
-        cars.add(new Car(3, "Audi", 2010));
-        cars.add(new Car(4, "Bently", 2000));
-        cars.add(new Car(5, "Acura", 1999));
         return cars.stream().limit(count).toList();
     }
 }
